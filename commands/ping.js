a451eb3f1b9364feecb153fe6f4c0cf1df755fec
xavierk09 <xaviervavierk@gmail.com>
2021-03-19 18:53:10 +0530

    fifth commit

--- commands/ping.js
+++ commands/ping.js
@@ -5,10 +5,6 @@ module.exports = {
     permissions: ["SEND_MESSAGES"],
     description: "This is a ping command!",
     async execute(message, args, cmd, client, Discord) {
-        const exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#D0D5D9')
-            .setTitle('✨ Pong!')
-            .setFooter("✨ Have a nice day!");
-        message.channel.send(exampleEmbed);
+        message.channel.send("Pong!");
     }
 }
